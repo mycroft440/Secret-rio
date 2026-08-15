e8d015dfc6a1593777a813da05af63786ca48c5f
mycroft440 <alquimistadodetalhe@gmail.com>
2026-08-15 16:07:45 -0300

    Order Gradle plugin configuration safely

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-plugins {
-    id("com.android.application") version "9.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
-}
-
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
     }
+}
+
+plugins {
+    id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
